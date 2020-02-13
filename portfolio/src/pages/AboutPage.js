7d37185f32f6c1fe0e12451059f7d968f342f33f
Tai Le <tle449@gmail.com>
2020-02-13 01:34:28 -0800

    added content.js and AboutMe page

---
+++ portfolio/src/pages/AboutPage.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Content from "../components/Content";
+
+function AboutPage(props) {
+    return(
+        <div>
+            <Content>
+                <p>I was born and lived in Vietnam for about 13 years. In 2008, my family decided to moved to the United States. It was an excited moment of my life, also a culture shock for me since I barely knew any English. So, I spent the first two years learning English. I graduted from Lincoln High School and got accepted into UCR in 2015. As you can see from the picture, I graduted college with a B.A in Business Economics.</p>
+                <p>I have been looking for jobs since graduation but it was a challenge. So I decided to join coding bootcamp at UCB Extension with the hope to increase job opportunities.</p>
+            </Content>
+        </div>
+    )
+}
+
+export default AboutPage;
