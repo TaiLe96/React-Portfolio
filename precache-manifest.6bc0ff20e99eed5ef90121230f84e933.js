c6bde4f47bc7b686ccb285c323b24981c4326a2c
Tai Le <tle449@gmail.com>
2020-02-15 20:53:38 -0800

    Updates

---
+++ precache-manifest.6bc0ff20e99eed5ef90121230f84e933.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cc2f5a41b9702d27001cf950db7b9b5",
+    "url": "/React-Portfolio/index.html"
+  },
+  {
+    "revision": "038177e7c126de6df632",
+    "url": "/React-Portfolio/static/css/2.31d1dd2a.chunk.css"
+  },
+  {
+    "revision": "a2230b45166590482b03",
+    "url": "/React-Portfolio/static/css/main.c17e69fe.chunk.css"
+  },
+  {
+    "revision": "038177e7c126de6df632",
+    "url": "/React-Portfolio/static/js/2.e8d5223f.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/React-Portfolio/static/js/2.e8d5223f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2230b45166590482b03",
+    "url": "/React-Portfolio/static/js/main.eee834da.chunk.js"
+  },
+  {
+    "revision": "bb0fd0ed5b911a89d2ae",
+    "url": "/React-Portfolio/static/js/runtime-main.c13bb608.js"
+  },
+  {
+    "revision": "ac199dc9019c6161374d573f5d779554",
+    "url": "/React-Portfolio/static/media/background2.ac199dc9.jpg"
+  },
+  {
+    "revision": "c3137b465cd75d6d8bd6b529fcb5cbaf",
+    "url": "/React-Portfolio/static/media/diner.c3137b46.png"
+  },
+  {
+    "revision": "a22ae3abc0656969940312fb11e6233e",
+    "url": "/React-Portfolio/static/media/notetaker.a22ae3ab.png"
+  },
+  {
+    "revision": "7b0ebae33452aa9ff82b3f2a0ab9be14",
+    "url": "/React-Portfolio/static/media/password.7b0ebae3.png"
+  },
+  {
+    "revision": "8c4ab5986029e29cafeb1c2e86a25b19",
+    "url": "/React-Portfolio/static/media/templateengine.8c4ab598.png"
+  },
+  {
+    "revision": "46f61da39be7d0740afb8e2c89cba5dc",
+    "url": "/React-Portfolio/static/media/tracker.46f61da3.png"
+  }
+]);
